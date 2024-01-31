122f6b48dcdcd7e7f5591bc0427d7428c8cc91ae
MSCMDD <MSCAoMe@outlook.com>
2024-01-31 23:46:59 +0800

    Update Url

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+export default {
+    themeConfig: {
+    siteTitle: '服务器集体宣传组织', //站点标题
+    logo: "/logo2.png",
+    description: '一个Minecraft服务器集体宣传组织',//mate标签description，多用于搜索引擎抓取摘要
+    nav: [
+        { text: "首页", link: "/" },
+    ],
+    socialLinks: [
+        { icon: "github", link: "https://gitee.com/geeksdidi" },
+        { icon: "twitter", link: "..." },
+    ],
+  },
+};
