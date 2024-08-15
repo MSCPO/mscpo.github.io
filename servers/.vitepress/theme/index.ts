b0297b949e879545f30c254908fb5fa4e03b733f
MSCMDD <MSCAoMe@outlook.com>
2024-08-16 00:32:19 +0800

    Added Hygeia(Remix) Hygeia(Remix) All rigts (are) reserved

--- servers/.vitepress/theme/index.ts
+++ servers/.vitepress/theme/index.ts
@@ -5,6 +5,10 @@ import Confetti from "./components/Confetti.vue"
 import Button from "./components/Button.vue"
 import { inBrowser } from 'vitepress'
 import busuanzi from 'busuanzi.pure.js'
+import vitepressMusic from 'vitepress-plugin-music'
+import 'vitepress-plugin-music/lib/css/index.css'
+import vitepressBackToTop from 'vitepress-plugin-back-to-top'
+import 'vitepress-plugin-back-to-top/dist/style.css'
 
 export default {
   ...VuetomTheme,
@@ -13,10 +17,24 @@ export default {
     app.component('Layout' , Layout)
     app.component('Confetti' , Confetti)
     app.component('Button' , Button)
+    
     if (inBrowser) {
       router.onAfterRouteChanged = () => {
         busuanzi.fetch()
       }
     }
-  }
+    vitepressMusic(playlist)
+    vitepressBackToTop({
+      // default
+      threshold:300
+    })
+  },
 }
+
+const playlist = [
+  {
+    name: 'Hygeia(Remix)',
+    author: 'MSCPO',
+    file: '/Hygeia(Remix).mp3',
+  },
+]
